cc840308c97813627e29f52fdf6f80be8f6027c2
gustavo.comim <gustavo.comim@evup.com.br>
2021-08-02 22:30:40 -0300

    Alteração primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,7 @@
 import { useState } from 'react'
 
 function Home() {
-    return <div>
-        <h2>Home 3</h2>
-        <Contador />
-    </div>
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1)
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar Contador</button>
-        </div>
-    )
+    return <div>Home</div>
 }
 
 export default Home
