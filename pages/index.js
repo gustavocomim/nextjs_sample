73b18c2fba1226b0d1ca54f547e657ab78eb7b38
gustavo.comim <gustavo.comim@evup.com.br>
2021-08-03 21:57:18 -0300

    Inclusão da tag Link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
-import { useState } from 'react'
+import Link from 'next/link'
 
 function Home() {
-    return <div>Home</div>
+    return <div>
+        <h1>Home</h1>
+        <Link href="/sobre">
+            <a>Acessar tela Sobre</a>
+        </Link>
+    </div>
 }
 
 export default Home
